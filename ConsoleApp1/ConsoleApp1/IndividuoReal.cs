0ce6e2249f672f8f5a32b7f347dcfabe5f41bef8
IvanSE10 <ivan.sanchez.enciso@gmail.com>
2018-03-14 14:25:21 -0600

    Clase individuo Real

---
+++ ConsoleApp1/ConsoleApp1/IndividuoReal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class IndividuoReal
+    {
+        CromosomaReal cromosomaReal;
+        //constructor de IndividuoReal, recibe el # de variables a utilizar, rango [valorMinimo, valorMaximo], y el numero de particiones
+        public IndividuoReal(int numVariables, int valorMinimo, int valorMaximo, int particiones)
+        {
+            cromosomaReal = new CromosomaReal(numVariables, valorMinimo, valorMaximo, particiones);
+        }
+        //representacion del cromosoma del individuo
+        public override string ToString()
+        {
+            return cromosomaReal.ToString();
+        }
+    }
+}
