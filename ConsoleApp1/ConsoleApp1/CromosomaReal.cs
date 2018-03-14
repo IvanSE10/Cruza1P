21c03272fc1e131f65a34ac15dfa7c3f9d9f4b84
IvanSE10 <ivan.sanchez.enciso@gmail.com>
2018-03-14 14:24:15 -0600

    agregar Individuo
    Agrega

---
+++ ConsoleApp1/ConsoleApp1/CromosomaReal.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class CromosomaReal
+    {
+        private GeneParticion[] genes;
+        private int min, max, P, numGenes, size;
+        private float dx;
+        //Constructor de CromosomaReal, recibe el # de genes a utilizar, el rango [min,max], y el numero de particiones
+        public CromosomaReal(int numGenes, int min, int max, int P)
+        {
+            this.numGenes = numGenes;
+            this.min = min;
+            this.max = max;
+            this.P = P;
+
+            size = (int)Math.Ceiling(Math.Log(P, 2)); // calculo de el tamaño de los Genes
+
+            genes = new GeneParticion[numGenes]; // creacion del arreglo de genes, de tamaño numGenes
+
+            dx = Math.Abs(min - max);
+            dx = dx / P; // calculo del incremento
+
+            initGenes();
+        }
+        //instancia #numGenes de tamaño size
+        private void initGenes()
+        {
+            for (int i = 0; i < numGenes; i++)
+            {
+                genes[i] = new GeneParticion(size);
+            }
+        }
+        //regresa el valor en representacion real del Gene
+        private float GetValorReal(GeneParticion g)
+        {
+            return min + dx * g.GetDecimal();
+        }
+        //Representacion de el cromosoma [binario] [gray]: valorReal 
+        public override string ToString()
+        {
+            string s = "Cromosomas:\n";
+            for (int i = 0; i < numGenes; i++)
+            {
+                s += "[" + genes[i].GetGrey() + "]: " + GetValorReal(genes[i]) + "\n";
+            }
+            return s;
+        }
+        //Regresa el incremento
+        public float GetIncremento()
+        {
+            return dx;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/GeneParticion.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class GeneParticion
+    {
+        private int[] gene;
+        //Constructor de GeneParticion, recibe el tamaño del gene
+        public GeneParticion(int size)
+        {
+            gene = new int[size];
+            Random random = new Random();
+            //se crea llena el gene de tamaño size con numeros aleatorios de 1´s y 0´s
+            for (int x = 0; x < size; x++)
+            {
+                gene[x] = random.Next(2);
+            }
+        }
+        //Representacion en binario del Gene
+        public string GetBinario()
+        {
+            string s = "";
+            for (int i = 0; i < gene.Length; i++)
+            {
+                s += gene[i];
+            }
+            return s;
+        }
+        //Representacion en codigo gray del Gene
+        public string GetGrey()
+        {
+            string s = "";
+            for (int i = 1; i < gene.Length; i++)
+            {
+                s += gene[i - 1] ^ gene[i];
+            }
+            return gene[0] + s;
+        }
+        //Valor en decimal del gene
+        public int GetDecimal()
+        {
+            int valor = 0;
+            for (int x = 0; x < gene.Length; x++)
+            {
+                valor += gene[x] * (int)Math.Pow(2, gene.Length - x - 1);
+            }
+            return valor;
+        }
+    }
+
+}
